e86326d6646cf4aa3065bb3542a3e02064ae5817
Luisangelsolis7 <lsolis01@aurora.edu>
2022-03-14 09:31:22 -0500

    updating relevant information

--- findme/src/DatabaseConnection.js
+++ findme/src/DatabaseConnection.js
@@ -3,10 +3,7 @@ let connection = mysql.createConnection({
     host: "45.55.136.114",
     user: "teamDB_F2021",
     password: "no1inTeam!",
-    database: "teamDB_F2021"
-});
-connection.connect(function(err) {
-    if (err) throw err;
-    console.log('Database is connected successfully !');
+    database: "teamDB_F2021_2"
 });
 module.exports = connection;
+export default connection;
--- findme/src/components/ItemList.js
+++ findme/src/components/ItemList.js
@@ -6,15 +6,28 @@ const ItemList = function(){
 
     return(
 <>
+    "itemName": "All About Money",
+    "itemCategory": "",
+    "itemDescription": "Its about the root of all evil",
+    "itemValue": "This ariticle is a complete guide about money. Its nice to have but not eveyone has it.",
+    "userFirst": "",
+    "userLast": "",
+    "userDOB": "",
+    "userPhone": "",
+    "userEmail": "",
+    "date": "",
+    "time": "",
+    "location": ""
+    }
                 <table className="table">
                     <thead className="table-dark">
                     <tr scope="row">
-                        <td>Blah</td>
-                        <td scope="col">Blah</td>
-                        <td scope="col">Blah</td>
-                        <td scope="col">Blah</td>
-                        <td scope="col">Blah</td>
-                        <td scope="col">Blah</td>
+                        <td scope="col">Name</td>
+                        <td scope="col">Category</td>
+                        <td scope="col">Description</td>
+                        <td scope="col">Value</td>
+                        <td scope="col">Date</td>
+                        <td scope="col">Location</td>
                     </tr>
                     </thead>
                     <tr>
@@ -25,9 +38,24 @@ const ItemList = function(){
                                     </div>
                         </td>
                         <td>Name</td>
-                        <td></td>
-                        <td>Value</td>
-                        <td>Blah Blah Blah DESC</td>
+                        <td><select className="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
+                            <option value="electronic">Electronic</option>
+                            <option value="clothing">Clothing</option>
+                            <option value="accessory">accessory</option>
+                            <option value="id">Identification</option>
+                            <option value="misc">Misc</option>
+                        </select></td>
+                        <td>This is a test description because we have no idea how long they are going to be. so i will continue to add words here</td>
+                        <td>$1000.00</td>
+                        <td>3/14/2022</td>
+                        <td><select>
+                            <option value="electronic">Stephens Hall</option>
+                            <option value="clothing">Parking Garage</option>
+                            <option value="accessory">Dunham Hall</option>
+                            <option value="id">Institute for Collaboration</option>
+                            <option value="misc">Business Office</option>
+                            <option value="misc"></option>
+                        </select></td>
                     </tr>
                     <tr>
                         <td>
