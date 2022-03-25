7723b7f4e0e93302d8fc9ad9690c1e4991281221
Luisangelsolis7 <lsolis01@aurora.edu>
2022-03-25 15:45:02 -0500

    Total report form rework

--- findme/src/components/ItemList.js
+++ findme/src/components/ItemList.js
@@ -2,125 +2,63 @@ import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import useFetch from "../useFetch";
 import React from 'react';
-const ItemList = function(props){
+
+const ItemList = function (props) {
     //This is the code to format how the list of items to be displayed
-    let locations = ["N/A","Alumni Hall","Bookstore","Business Office","Campus Public Safety","Business Office","Centennial Hall",
-        "Parking Garage","Dunham Hall","Institute for Collaboration","Business Office",
-        "The Community Foundation of the Fox River Valley Center for Cultural Enrichment and Perry Theatre",
-        "Copy Center","Dunham Hall","Davis Hall","Dining Hall","Dunham Hall","Eckhart Hall","Fitness Center","Founders Annex",
-        "Founders House","Hill Welcome Center and Ethel Tapper Recital Hall","Human Resources","Institute for Collaboration",
-        "Crimi Auditorium","Jenks Hall","John C. Dunham STEM Partnership School","Kimberly and James Hill Center for Student Success",
-        "Labyrinth","Mail Center","Memorial Hall","Michael J. Birck Collaboration Center for Innovation",
-        "Office of Admission and Financial Aid","Phillips Library","Quad","Roger and Marilyn Parolini Music Center",
-        "Schingoethe Center","Spartan Spot","Spartan Statue/Bedrosian Plaza","Spartan Terrace","Stephens Hall",
-        "Thornton Gymnasium","Tru Blu Coffee","University Banquet Hall","University Communications","Vago Field","Parking Garage",
-        "Wackerlin Center for Faith and Action","Watkins Hall","Wellness Center","Wilkinson Hall"];
     let tableHeader = [];
-    if(props.active == "H"){
-        tableHeader = ["","Name","Category","Description","Value","Date","Location","Officer"];
-        return(<>
-            <table className="table">
-                <thead className="table-dark">
-                <tr scope="row">
-                    {tableHeader.map((i) => (
-                        <td key={i}>{i}</td>
-                    ))}
-                </tr>
-                </thead>
-                <tbody>
 
-
-                {props.items.map((i) => (
-                    <tr>
-                        <td><div className="input-group-text">
-                            <input className="form-check-input mt-0" type="checkbox" value=""
-                                   aria-label="Checkbox for following text input"></input>
-                        </div></td>
-                        <td>{i.Item_Name}</td>
-                        <td>{i.Category_Name}</td>
-                        <td>{i.Item_Desc}</td>
-                        <td>${i.Item_Value}</td>
-                        <td>{i.ISH_Date}</td>
-                        <td>{i.ISH_Location}</td>
-                        <td>{i.Officer_Fname} {i.Officer_Lname} <br/>{i.Officer_Badge}</td>
-
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-        </>)
-    }if(props.active == "C"){
-
-        tableHeader =["","Name", "Category", "Description", "Value", "Date", "Claimant",
-            "Officer"];
-        return(
-            <>                <table className="table">
-                <thead className="table-dark">
-                <tr scope="row">
-                    {tableHeader.map((i) => (
-                        <td key={i}>{i}</td>
-                    ))}
-                </tr>
-                </thead>
-                <tbody>
-                {props.items.map((i) => (
-                    <tr>
-                        <td><div className="input-group-text">
-                            <input className="form-check-input mt-0" type="checkbox" value=""
-                                   aria-label="Checkbox for following text input"></input>
-                        </div></td>
-                        <td>{i.Item_Name}</td>
-                        <td>{i.Category_Name}</td>
-                        <td>{i.Item_Desc}</td>
-                        <td>${i.Item_Value}</td>
-                        <td>{i.ISH_Date}</td>
-                        <td>{i.User_Fname} {i.User_Lname} <br/>
-                            {i.User_Email} <br/>
-                            {i.User_Phone}</td>
-                        <td>{i.Officer_Fname} {i.Officer_Lname} <br/> Badge: {i.Officer_Badge}</td>
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-            </>
-        )
-    }if(props.active == "R"){
-
-        tableHeader =["","Item", "Category", "Description", "Value", "Date", "Location", "Reported by"];
-        return(
-            <>                <table className="table">
-                <thead className="table-dark">
-                <tr scope="row">
-                    {tableHeader.map((i) => (
-                        <td key={i}>{i}</td>
-                    ))}
-                </tr>
-                </thead>
-                <tbody>
-                {props.items.map((i) => (
-                    <tr>
-
-                        <td><div className="input-group-text">
-                            <input className="form-check-input mt-0" type="checkbox" value=""
-                                   aria-label="Checkbox for following text input"></input>
-                        </div></td>
-                        <td>{i.Item_Name}</td>
-                        <td>{i.Category_Name}</td>
-                        <td>{i.Item_Desc}</td>
-                        <td>${i.Item_Value}</td>
-                        <td>{i.ISH_Date}</td>
-                        <td>{i.ISH_Location}</td>
-                        <td>{i.User_Fname} {i.User_Lname} <br/>
-                        {i.User_Email} <br/>
-                        {i.User_Phone}</td>
-
-                    </tr>
-                ))}
-                </tbody>
-            </table>
-            </>
-        )
+    function checkNull(item) {
+        if (item !== null) {
+            if(!isNaN(+item)){
+                return(<>Badge: {item}</>)
+            }
+            return (<>{item}</>)
+        }
     }
+
+    if (props.active == "H") {
+        tableHeader = ["", "Name", "Category", "Description", "Value", "Date", "Location","", "Officer"];
+    }
+    if (props.active == "C") {
+        tableHeader = ["", "Name", "Category", "Description", "Value", "Date","", "Claimant", "Officer"];
+    }
+    if (props.active == "R") {
+        tableHeader = ["", "Item","Category", "Description", "Value", "Date", "Location", "Reported by",""];
+    }
+
+    return(
+        <>                <table className="table">
+            <thead className="table-dark">
+            <tr scope="row">
+                {tableHeader.map((i) => (
+                    <td key={i}>{i}</td>
+                ))}
+            </tr>
+            </thead>
+            <tbody>
+            {props.items.map((i) => (
+                <tr>
+                    <td><div className="input-group-text">
+                        <input className="form-check-input mt-0" type="checkbox" value=""
+                               aria-label="Checkbox for following text input"></input>
+                    </div></td>
+                    <td>{checkNull(i.Item_Name)}</td>
+                    <td>{checkNull(i.Category_Name)}</td>
+                    <td>{i.Item_Desc}</td>
+                    <td>${i.Item_Value}</td>
+                    <td>{i.ISH_Date}</td>
+                    <td>{checkNull(i.ISH_Location)}</td>
+                    <td>{checkNull(i.User_Fname)} {checkNull(i.User_Lname)} <br/>
+                        {checkNull(i.User_Email)} <br/>
+                        {checkNull(i.User_Phone)}</td>
+                    <td>{checkNull(i.Officer_Fname)} {checkNull(i.Officer_Lname)} <br/> {checkNull(i.Officer_Badge)}</td>
+                </tr>
+            ))}
+            </tbody>
+        </table>
+        </>
+
+    )
 
 }
 export default ItemList;
--- findme/src/components/Modal.js
+++ findme/src/components/Modal.js
@@ -6,47 +6,51 @@ function Modal(props){
     if(!props.show){
         return null;
     }
-    return(
-        <div className="modal">
-            <div className="modal-content">
-                <div className="modal-header">
-                    <h4 className="modal-title">
-                        This is a title
-                    </h4>
-                </div>
-                <div className="modal-body">
-                    <Container>
-                        <Row>
-                            Item Name:<input type="text" />
-                        </Row>
-                        <Row>
-                            Item Category:<select className="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
-                            <option value="electronic">Electronic</option>
-                            <option value="clothing">Clothing</option>
-                            <option value="accessory">accessory</option>
-                            <option value="id">Identification</option>
-                            <option value="misc">Misc</option>
-                        </select>
-                        </Row>
-                        <Row>
-                            Description:
-                            <input type="typearea" />
-                        </Row>
-
-                        <Row>
-                            Location:
-                            <select>
-
+    if(props.active == "H"){
+        return(
+            <div className="modal">
+                <div className="modal-content">
+                    <div className="modal-header">
+                        <h4 className="modal-title">
+                            This is a title
+                        </h4>
+                    </div>
+                    <div className="modal-body">
+                        <Container>
+                            <Row>
+                                Item Name:<input type="text" />
+                            </Row>
+                            <Row>
+                                Item Category:<select className="form-select" id="inputGroupSelect04" aria-label="Example select with button addon">
+                                <option value="electronic">Electronic</option>
+                                <option value="clothing">Clothing</option>
+                                <option value="accessory">accessory</option>
+                                <option value="id">Identification</option>
+                                <option value="misc">Misc</option>
                             </select>
-                        </Row>
-                    </Container>
-                </div>
-                <div className="modal-footer">
-                    <button>Add</button>
-                    <button onClick={props.onClose}>Close</button>
+                            </Row>
+                            <Row>
+                                Description:
+                                <input type="typearea" />
+                            </Row>
+
+                            <Row>
+                                Location:
+                                <select>
+
+                                </select>
+                            </Row>
+                        </Container>
+                    </div>
+                    <div className="modal-footer">
+                        <button>Add</button>
+                        <button onClick={props.onClose}>Close</button>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+
+    }
+
 }
 export default Modal;
