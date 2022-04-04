c9c9cf2a91fce834eddf0c1313eeb7355633349d
jswartz12 <jswartz04@aurora.edu>
2022-04-04 13:13:04 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,6 +8,24 @@ app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+app.get("/api/getLost", (req, res) => {
+    const sql = `SELECT i.Item_ID, c.Category_Name, i.Item_Name, i.Item_Value, i.Item_Desc, 
+                        ish.Status_FK, ish.ISH_Location, ish.ISH_Date, ish.ISH_Time, 
+                        u.User_Fname, u.User_Lname, u.User_DOB, u.User_DL, u.User_Email, u.User_Phone
+                  FROM (Select h.Item_FK, h.ISH_Date, h.Status_FK, h.User_FK, h.ISH_Location, h.ISH_Time 
+                        From Item_Status_History h, (Select Item_FK, MAX(ISH_Date) as mdate 
+                        from Item_Status_History Group by Item_FK) t where t.Item_FK = h.Item_FK and t.mdate = h.ISH_date) ish 
+                  Join Item i on i.Item_ID = ish.Item_FK 
+                  Left Join User u on u.User_ID = ish.User_FK join Category c on i.Category_FK = c.Category_Name Where ish.Status_FK = 'Lost' 
+                  Order by ish.ISH_Date DESC`;
+    db.query(sql, (err, result)=>{
+        if(err){
+            console.log(err)
+        }
+        res.send(result);
+    });
+});
+
 app.get("/api/getUnclaimed", (req, res) => {
     const sql = `SELECT i.Item_ID, c.Category_Name, i.Item_Name, i.Item_Value, i.Item_Desc, 
                         ish.Status_FK, ish.ISH_Location, ish.ISH_Date, ish.ISH_Time, 
@@ -49,24 +67,6 @@ app.get("/api/getClaimed", (req, res) => {
     });
 });
 
-app.get("/api/getLost", (req, res) => {
-    const sql = `SELECT i.Item_ID, c.Category_Name, i.Item_Name, i.Item_Value, i.Item_Desc, 
-                        ish.Status_FK, ish.ISH_Location, ish.ISH_Date, ish.ISH_Time, 
-                        u.User_Fname, u.User_Lname, u.User_DOB, u.User_DL, u.User_Email, u.User_Phone
-                  FROM (Select h.Item_FK, h.ISH_Date, h.Status_FK, h.User_FK, h.ISH_Location, h.ISH_Time 
-                        From Item_Status_History h, (Select Item_FK, MAX(ISH_Date) as mdate 
-                        from Item_Status_History Group by Item_FK) t where t.Item_FK = h.Item_FK and t.mdate = h.ISH_date) ish 
-                  Join Item i on i.Item_ID = ish.Item_FK 
-                  Left Join User u on u.User_ID = ish.User_FK join Category c on i.Category_FK = c.Category_Name Where ish.Status_FK = 'Lost' 
-                  Order by ish.ISH_Date DESC`;
-    db.query(sql, (err, result)=>{
-        if(err){
-            console.log(err)
-        }
-        res.send(result);
-    });
-});
-
 app.post("/api/insertLost", (req, res) => {
 
     const userSql = `INSERT INTO User (User_Fname, User_Lname, User_Phone, User_Email) values (?, ?, ?, ?);`
@@ -92,24 +92,69 @@ app.post("/api/insertLost", (req, res) => {
     });
 });
 
+app.post("/api/insertUnclaimed", (req, res) => {
+
+    const itemSql = `INSERT INTO Item (Item_Name, Category_FK, Item_Value, Item_Desc) values (?, ?, ?, ?);`
+    const ishSql = `INSERT INTO Item_Status_History (Item_FK, Officer_FK, Status_FK, ISH_Date, ISH_Time, ISH_Location) values (?, '999', 'Unclaimed', ?, ?, ?);`
+
+        db.query(itemSql, [req.body.itemName, req.body.category, req.body.value, req.body.desc], (err, iResult) => {
+            const itemId = iResult.insertId;
+            if(err){
+                console.log(err)
+            }
+            db.query(ishSql, [itemId, req.body.date, req.body.time, req.body.location], (err, result) => {
+                if(err){
+                    console.log(err)
+                }
+            });
+        });
+});
+
+app.post("/api/insertClaimed", (req, res) => {
+
+    const itemId = req.body.itemId;
+    const userSql = `INSERT INTO User (User_Fname, User_Lname, User_Phone, User_Email) values (?, ?, ?, ?);`
+    const ishSql = `INSERT INTO Item_Status_History (Item_FK, User_FK, Officer_FK, Status_FK, ISH_Date, ISH_Time, ISH_Location) values (?, ?, '999', 'Claimed', ?, ?, ?);`
+
+    db.query(userSql, [req.body.firstName, req.body.lastName, req.body.phone, req.body.email], (err, uResult) => {
+        const userId = uResult.insertId;
+        if(err){
+            console.log(err)
+        }
+            db.query(ishSql, [itemId, userId, req.body.date, req.body.time, req.body.location], (err, result) => {
+                if(err){
+                    console.log(err)
+                }
+            });
+    });
+});
+
 app.post("api/edit"), (req,res) => {
     const sql = `UPDATE Item
-                 SET Item_Name = ?, Item_Category = ?, Item_Value = ?, Item_Desc = ?
+                 SET Item_Name = ?, Category_FK = ?, Item_Value = ?, Item_Desc = ?
                  Where Item_ID = ?`
     db.query(sql, [req.body.itemName, req.body.category, req.body.value, req.body.desc, req.body.itemId], (err, result) =>{
-        if(req.body.category = 'Lost'){
+        if(err){
+            console.log(err);
+        }
+        if(req.body.category == 'Lost' || req.body.category == 'Claimed'){
             const uSql = `UPDATE User
                           SET User_Fname = ?, User_Lname = ?, User_Phone = ?, User_Email = ?
                           Where User_ID = ?`
-            db.query(uSql, [req.body.firstName, req.body.lastName, req.body.email, req.body.phone, req.body.userId]), (err, result) => {
+            db.query(uSql, [req.body.firstName, req.body.lastName, req.body.phone, req.body.email, req.body.userId]), (err, result) => {
                 if(err){
                     console.log(err);
                 }
             }
-            if(err){
-                console.log(err);
-            }
         }
+        const ishSql = `Update Item_Status_History
+                      SET ISH_Date = ?, ISH_Time = ?, ISH_Location = ?
+                      WHERE Item_FK = ? and Status_FK = ?`
+        db.query(ishSql, [req.body.date, req.body.time, req.body.location, req.body.itemId, req.body.category], (err, result) =>{
+            if(err){
+                console.log(err)
+            }
+        })
 
     })
 }
