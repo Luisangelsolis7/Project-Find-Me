53c34d3cc97615ffeb8e5be09ce0d5eb9ea3baec
Luisangelsolis7 <lsolis01@aurora.edu>
2022-04-28 19:58:47 -0500

    Update allowedOrigins.js

--- server/config/allowedOrigins.js
+++ server/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3001',
     'http://localhost:3000',
-    "*"
+    "https://lost-n-found-69q4b.ondigitalocean.app/"
 ]
 
 module.exports = allowedOrigins;
