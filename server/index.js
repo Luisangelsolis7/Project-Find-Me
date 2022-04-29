8830c745b9c291fab2ae07a4cab530e88b1bac69
jswartz12 <jswartz04@aurora.edu>
2022-04-28 20:45:27 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -19,6 +19,17 @@ app.use(express.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
 
+if (process.env.NODE_ENV === 'production') {
+    // Exprees will serve up production assets
+    app.use(express.static('client/build'));
+
+    // Express serve up index.html file if it doesn't recognize route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 app.get("/api/getLost", authenticateToken, (req, res) => {
     const sql = `SELECT i.Item_ID, c.Category_Name, i.Item_Name, i.Item_Value, i.Item_Desc, 
                         ish.Status_FK, ish.ISH_Location, ish.ISH_Date, ish.ISH_Time, ish.User_FK,
