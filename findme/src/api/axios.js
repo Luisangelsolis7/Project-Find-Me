08758aabc41231c5b70ebcee62acc564ec983888
jswartz12 <jswartz04@aurora.edu>
2022-04-29 11:57:51 -0500

    Update axios.js

--- findme/src/api/axios.js
+++ findme/src/api/axios.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = 'https://findme-56jcg.ondigitalocean.app/'
+const BASE_URL = 'https://findme-56jcg.ondigitalocean.app:8080/'
 
 export default axios.create({
     baseURL: BASE_URL
